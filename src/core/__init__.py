2093e43537371b4b1296f465b4b55e782bde67e7
vastelhanter <vastelhanter@gmail.com>
2025-12-29 20:23:37 +0300

    -feat: deleted path for web-parsing in Search module

--- src/core/__init__.py
+++ src/core/__init__.py
@@ -18,23 +18,23 @@ class Core:
      try:
 
     #  получаем данные пользователя
-      user_data = self.input_manager.run()
+      # user_data = self.input_manager.run()
     
-    #   user_data = {
-    #     'type_search': 'deep',
-    #     'mode_work': 'fs',
-    #     'area_work': '/home/maksim/Тест',
-    #     'regex_list': ['def',ready_regex['date']],
-    #     'extensions_list':['.py','.txt','.md'],
-    #     'output_place': '/home/maksim/Тест/result.txt'
-    #  }
+      user_data = {
+        'type_search': 'deep',
+        'mode_work': 'web',
+        'area_work': 'https://www.avito.ru/velikiy_novgorod/remont_i_stroitelstvo/drova_-_kolotye_i_churkami_7830301578?context=H4sIAAAAAAAA_wEmANn_YToxOntzOjE6IngiO3M6MTY6IkhJQkV5MTRaQmt2b2hsdmwiO30kpFYKJgAAAA',
+        'regex_list': ['Ford Explorer'],
+        'extensions_list':['.py','.txt','.md'],
+        'output_place': '/home/maksim/Тест/result.txt'
+     }
     
     # поиск
       result_search = Search(user_data).run()
 
     
     # вывод результатов
-      output.run(result_search, user_data['output_place'])
+      # output.run(result_search, user_data['output_place'])
 
     #  print(user_data)
      except KeyboardInterrupt:
--- src/modules/search/__init__.py
+++ src/modules/search/__init__.py
@@ -1,151 +1,130 @@
-import os,re
+import os
+import re
 
 from ..styled_print import styled_print
 
-# alias
-join = os.path.join
 
 class Search:
+    def __init__(self, user_data):
+        self.data = user_data
 
-  def __init__(self, user_data):
-
-    self.data = user_data
-  
-  # функция для поиска файлов в директории, возвращает массив файлов
-  def get_files(self,root_directory,type_search,extensions):
-
-    """
-    Docstring для get_files
-    
-    :param root_directory: начальная директория, откуда начинается поиск
-    :param type_search: тип поиска
-    """
-
-     # инициализируем списки для файлов и дерево
-    files = []
-
-    if type_search == 'deep':
-
-     while True:
+    # функция для поиска файлов в директории, возвращает массив файлов
+    def get_files(self, root_directory, type_search, extensions):
+        """
+        Docstring для get_files
         
-        # получаем содержимое директории
-        list_all = os.listdir(root_directory)
+        :param root_directory: начальная директория, откуда начинается поиск
+        :param type_search: тип поиска
+        """
 
-        # инициализируем список папок
-        dirs = []
+        # инициализируем списки для файлов и дерево
+        files = []
 
-        # проходимся по каждому элементу директории
-        for index,element in enumerate(list_all):
+        if type_search == 'deep':
+            while True:
+                # получаем содержимое директории
+                list_all = os.listdir(root_directory)
 
-            # определяем файл или папка
-            if os.path.isfile(join(root_directory,element)):
-                
-                name, extension = os.path.splitext(join(root_directory,element))
+                # инициализируем список папок
+                dirs = []
 
-                # если расширение файла есть в переданных
-                if extension in extensions:
-                   
-                 files.append(join(root_directory,element))
+                # проходимся по каждому элементу директории
+                for index, element in enumerate(list_all):
+                    # определяем файл или папка
+                    if os.path.isfile(os.path.join(root_directory, element)):
+                        name, extension = os.path.splitext(
+                            os.path.join(root_directory, element)
+                        )
 
-            elif os.path.isdir(join(root_directory,element)):
-                dirs.append(element)
+                        # если расширение файла есть в переданных
+                        if extension in extensions:
+                            files.append(os.path.join(root_directory, element))
 
-        if len(dirs) == 0:
-            break
+                    elif os.path.isdir(os.path.join(root_directory, element)):
+                        dirs.append(element)
 
-        # если были папки в директории
-        else:
+                if len(dirs) == 0:
+                    break
 
-            # проходимся по всем папкам
-            for dir in dirs:
-                # проходимся глубоким поиском принимая данную папку за корневую
-                result = self.get_files(join(root_directory, dir),'deep',extensions=extensions)
+                # если были папки в директории
+                else:
+                    # проходимся по всем папкам
+                    for dir in dirs:
+                        # проходимся глубоким поиском принимая данную папку за корневую
+                        result = self.get_files(
+                            os.path.join(root_directory, dir),
+                            'deep',
+                            extensions=extensions
+                        )
 
-                # добавляем результат в найденные ранее файлы
-                files = files + result
+                        # добавляем результат в найденные ранее файлы
+                        files = files + result
 
+                break
 
+        elif type_search == 'shallow':
+            files = os.listdir(root_directory)
 
-        break
-     
-    elif type_search == 'shallow':
-       
-       files = os.listdir(root_directory)
+        return files
 
-    return files
-  
-  def scan_file(self,path_file, regex_list):
-     
-    #  инициализируем результат
-     result = {}
+    def scan_file(self, path_file, regex_list):
+        # инициализируем результат
+        result = []
 
-    #  читаем файл
-     with open(path_file,'r') as file:
-        
-        result.setdefault(path_file,[])
-        
-        # разбираем файл на строки
-        for index,line  in enumerate(file):
-           
-          #  проходимся по каждому регулярному выражению
-           for regex in regex_list:
-            
-            search_result = re.findall(regex,line)
+        # читаем файл
+        with open(path_file, 'r') as file:
+            result.setdefault(path_file, [])
 
-            if search_result:
-              
-              result[path_file].append(
-                 {
-                    f'{index + 1}': line
-                 }
-              )
-              
-        
-        file.close()
-      
-     return result
-  
-  # поиск по файловой системе
-  def search_fs(self,directory, type_search, regex_list, output_place, extensions_list):
+            # разбираем файл на строки
+            for index, line in enumerate(file):
+                # проходимся по каждому регулярному выражению
+                for regex in regex_list:
+                    search_result = re.findall(regex, line)
 
-    styled_print.info('Поиск...')
+                    if search_result:
+                        result.append(
+                            {
+                                f'{index + 1}': line
+                            }
+                        )
 
-    # получаем все файлы
-    all_files = self.get_files(directory, type_search, extensions=extensions_list)
+            file.close()
 
-    all_results = {}
+        return result
 
-    for file in all_files:
-       full_path = join(directory, file)
+    # поиск по файловой системе
+    def search_fs(self, directory, type_search, regex_list, extensions_list):
+        styled_print.info('Поиск...')
 
-       all_results[full_path] = self.scan_file(full_path,regex_list)
-       
-
-    return all_results
-
-  def run(self):
-
-    user_data = self.data
-
-    # инициализируем результат
-    result = {}
-
-    # ветвление по типу работы
-
-    if user_data['mode_work'] == 'fs':
-
-      result = self.search_fs(
-         directory=user_data['area_work'],
-         type_search=user_data['type_search'],
-         regex_list=user_data['regex_list'],
-         output_place=user_data['output_place'],
-         extensions_list=user_data['extensions_list']
+        # получаем все файлы
+        all_files = self.get_files(
+            directory,
+            type_search,
+            extensions=extensions_list
         )
-           
-    
-    else:
 
-      print('Веб-анализ')
-    
-    styled_print.success('Операция поиска успешно завершена')
-    return result
+        all_results = {}
+
+        for file in all_files:
+            full_path = os.path.join(directory, file)
+            all_results[full_path] = self.scan_file(full_path, regex_list)
+
+        return all_results
+
+    def run(self):
+        user_data = self.data
+
+        # инициализируем результат
+        result = {}
+
+        # ветвление по типу работы
+        if user_data['mode_work'] == 'fs':
+            result = self.search_fs(
+                directory=user_data['area_work'],
+                type_search=user_data['type_search'],
+                regex_list=user_data['regex_list'],
+                extensions_list=user_data['extensions_list']
+            )
+
+        styled_print.success('Операция поиска успешно завершена')
+        return result
